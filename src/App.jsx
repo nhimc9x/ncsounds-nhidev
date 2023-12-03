fd59899acd29f1ac08ebea07922349b4dd2da21a
nhimc9x <nhicomputer77@gmail.com>
2023-12-03 15:55:53 +0700

    Base

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { GiCarWheel } from 'react-icons/gi'
+
+function App() {
+  return (
+    <div className="">
+      <h1>Hello</h1>
+      <h1 className="text-3xl font-bold text-red-600 underline ">
+        Hello world!
+      </h1>
+      <h1 className="my-4 ml-10 bg-gray-600 p-10 text-xl uppercase tracking-widest text-white">
+        Xin chao VietNam
+      </h1>
+      <span className="h-[100px] w-[200px] block m-5 my-3 mt-2 p-2 px-4">hẹanscjasnjc <GiCarWheel color='#dc2626' fontSize='80px'/></span>
+    </div>
+  )
+}
+
+export default App
