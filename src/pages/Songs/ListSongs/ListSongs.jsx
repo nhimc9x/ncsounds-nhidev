20cb2d2aa9b689cb921ae8adf46c374cba158b40
nhimc9x <nhicomputer77@gmail.com>
2023-12-20 22:32:08 +0700

    Build Page Songs #2

--- src/pages/Songs/ListSongs/ListSongs.jsx
+++ src/pages/Songs/ListSongs/ListSongs.jsx
@@ -5,58 +5,112 @@ import { GoHeartFill } from 'react-icons/go'
 import { IoAlbums } from 'react-icons/io5'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import TooltipCustom from '~/components/TooltipCustom/TooltipCustom'
+import { useEffect, useState } from 'react'
+import './ListSongs.css'
 
 function ListSongs({ dataApis }) {
-  let listSongs = [...dataApis]
+
+  // Các lựa chọn sắp xếp
+  const SORTED_BY = {
+    NAME: 'name',
+    VIEWS: 'views',
+    LATEST: 'latest'
+  }
+
+  const [sortedSongs, setSortedSongs] = useState([...dataApis])
+  const [sortType, setSortType] = useState(null)
+
+  // Sắp xếp danh sách
+  useEffect(() => {
+    const sorted = [...dataApis]
+    switch (sortType) {
+    case SORTED_BY.NAME: {
+      sorted.sort((a, b) => {
+        if (a['name'] < b['name']) return -1
+        if (a['name'] > b['name']) return 1
+        return 0
+      })
+    } break
+
+    case SORTED_BY.VIEWS: {
+      sorted.sort((a, b) => {
+        if (b['views'] < a['views']) return -1
+        if (b['views'] > a['views']) return 1
+        return 0
+      })
+    } break
+
+    case SORTED_BY.LATEST: sorted.reverse()
+      break
+    }
+    setSortedSongs(sorted)
+  }, [sortType])
+
+  // Active button
+  const isActiveType = (type) => {
+    return type === sortType
+  }
+
   return (
-    <div className="2xl:px-10 xl:px-4 mdl:px-0 mt-4">
-      {listSongs.map((data, index) => (
-        <div key={index} className="h-[52px] py-1 px-3 border-ncs-secondary-color border-t last:border-b hover:bg-ncs-secondary-color flex justify-between items-center group">
-          <div className="text-ncs-text-color flex items-center gap-3">
-            <div className="">
-              <LuMusic />
-            </div>
-            <div className="w-9 h-9 relative">
-              <img className="w-full h-full rounded-sm object-cover group-hover:brightness-50" src={data?.links?.images[1]?.url} alt="" />
-              <div className="hidden group-hover:block text-sm absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
-                <FaPlay />
-              </div>
-            </div>
-            <div className="">
-              <div className="text-sm text-white">{data.name}</div>
-              <div className="text-xs">{data.author}</div>
-            </div>
-          </div>
+    <div className="mt-4">
 
-          <div className="flex items-center text-base text-ncs-text-color gap-1">
+      <div className="text-xl font-semibold tracking-wide text-white">
+        All songs
+      </div>
+      <div className="2xl:px-10 xl:px-4 mdl:px-0 mt-2">
 
-            <TooltipCustom decription="Add to favourite">
-              <div className="sms:flex hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
-                <GoHeartFill />
-              </div>
-            </TooltipCustom>
-            <TooltipCustom decription="Add to album">
-              <div className="sms:flex hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
-                <IoAlbums />
-              </div>
-            </TooltipCustom>
-            <TooltipCustom decription="Download">
-              <a href={data.url} className="sms:flex hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
-                <MdCloudDownload />
-              </a>
-            </TooltipCustom>
-
-            <TooltipCustom decription="Show more">
-              <div className="flex sms:hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
-                <BsThreeDotsVertical />
-              </div>
-            </TooltipCustom>
-
-          </div>
+        <div className="text-xs leading-[20px] text-white flex items-center gap-3 mb-2">
+          <div onClick={() => setSortType(SORTED_BY.NAME)} className={`sort-menu-item ${isActiveType(SORTED_BY.NAME) && 'bg-ncs-active-color text-black'}`}>Name</div>
+          <div onClick={() => setSortType(SORTED_BY.VIEWS)} className={`sort-menu-item ${isActiveType(SORTED_BY.VIEWS) && 'bg-ncs-active-color text-black'}`}>Most Views</div>
+          <div onClick={() => setSortType(SORTED_BY.LATEST)} className={`sort-menu-item ${isActiveType(SORTED_BY.LATEST) && 'bg-ncs-active-color text-black'}`}>Latest</div>
         </div>
-      ))}
 
+        {sortedSongs.map((data, index) => (
+          <div key={index} className="h-[52px] py-1 px-3 border-ncs-secondary-color border-t last:border-b hover:bg-ncs-secondary-color flex justify-between items-center group">
+            <div className="text-ncs-text-color flex items-center gap-3">
+              <div className="">
+                <LuMusic />
+              </div>
+              <div className="w-9 h-9 relative">
+                <img className="w-full h-full rounded-sm object-cover group-hover:brightness-50" src={data?.links?.images[1]?.url} alt="" />
+                <div className="hidden group-hover:block text-sm absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+                  <FaPlay />
+                </div>
+              </div>
+              <div className="">
+                <div className="text-sm text-white">{data.name}</div>
+                <div className="text-xs">{data.author}</div>
+              </div>
+            </div>
 
+            <div className="flex items-center text-base text-ncs-text-color gap-1">
+
+              <TooltipCustom decription="Add to favourite">
+                <div className="sms:flex hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
+                  <GoHeartFill />
+                </div>
+              </TooltipCustom>
+              <TooltipCustom decription="Add to album">
+                <div className="sms:flex hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
+                  <IoAlbums />
+                </div>
+              </TooltipCustom>
+              <TooltipCustom decription="Download">
+                <a href={data.url} className="sms:flex hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
+                  <MdCloudDownload />
+                </a>
+              </TooltipCustom>
+
+              <TooltipCustom decription="Show more">
+                <div className="flex sms:hidden h-7 w-7 hover:bg-ncs-primary-color rounded-full justify-center items-center">
+                  <BsThreeDotsVertical />
+                </div>
+              </TooltipCustom>
+
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
